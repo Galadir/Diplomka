d1339f0ba576e02ec40ae35d80073c215fec4fd2
Daniel <danpumr@gmail.com>
2021-09-17 12:36:38 +0200

    zabaleno do funkce jeden tvar bez souřadnic

---
+++ pokusy.py
@@ -0,0 +1,7 @@
+def scitani(x,y):
+    z=x+y
+    print(z)
+    return z
+
+z= scitani(5,6)
+print(z)
--- tvar_ ze_seznamu.py
+++ tvar_ ze_seznamu.py
@@ -4,50 +4,62 @@ import arcpy
 arcpy.env.workspace = "C:\\Users\\danie\\OneDrive\\Dokumenty\\ArcGIS\\Projects\\Diplomka\\Diplomka.gdb"
 arcpy.env.overwriteOutput = 1
 
-def UmisteniTvaru(tvar,souradnice,epsg,output):"""
-Funkce zadaná tvary rozmístí jako jednotlivé polygony na zadané souřadnice
+def UmisteniTvaru(tvar,souradnice,epsg,output):
+    """
+    Funkce zadaná tvary rozmístí jako jednotlivé polygony na zadané souřadnice
     :param tvar: Tvar definovaný souřadnicemi v seznamu
     :param souradnice: Souřadnice bodů
     :param epsg: Souřadnicový systém definovaný EPSG
     :param output: Feature Layer, do které se polygony zapíšou
     :return: 
     """
+
+    #definování outputu
     sr = arcpy.SpatialReference(epsg) # EPSG kod
     if not arcpy.Exists(output):
         arcpy.Delete_management(output)
     arcpy.CreateFeatureclass_management(arcpy.env.workspace, output, "POLYGON", "#", "#", "#", sr)
     arcpy.AddField_management(output,"VALUE","SHORT")
 
+    #InsertCursor, kterým budu vkládat výsledné tvary
+    insCur = arcpy.da.InsertCursor(output,["SHAPE@","VALUE"])
+
+    # vytvoření pole, do kterého nahraji souřadnice
+    part = arcpy.Array()
+    #proměnná, do které budu ukládat jednotlívé tvary
+    features = []
+
+    #nahrání tvaru do pole
+    for i in tvar:
+        pnt = arcpy.Point(i[0],i[1])
+        part.add(pnt)
+
+    #vytvoření polygonu z pole
+    polygon = arcpy.Polygon(part)
+    #vyčištění pole
+    #part.removeAll()
+    #features.append(polygon)
+
+    print(polygon)
+    insCur.insertRow((polygon,1))
+    del insCur
+
+
 output = "tvar_pokus"
-
-
+epsg = 32633
 tvar = [
-    [0,30],
+    [0,20],
     [0,0],
     [30,0],
     [30,30],
-    [0,30]
+    [0,20]
 ]
-#InsertCursor, kterým budu vkládat výsledné tvary
-insCur = arcpy.da.InsertCursor(output,["SHAPE@","VALUE"])
 
-#proměnná, do které budu ukládat jednotlívé tvary
-#features = []
 
-#vytvoření pole, do kterého nahraji souřadnice
-part = arcpy.Array()
+UmisteniTvaru(tvar,0,epsg,output)
 
-#nahrání tvaru do pole
-for i in tvar:
-    pnt = arcpy.Point(i[0],i[1])
-    part.add(pnt)
 
-#vytvoření polygonu z pole
-polygon = arcpy.Polygon(part)
-#vyčištění pole
-#part.removeAll()
-#features.append(polygon)
 
-print(polygon)
-insCur.insertRow((polygon,1))
-del insCur
+
+
+
